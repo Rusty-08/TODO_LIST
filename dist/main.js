d1fd721de912d233a77b296dbe26d71bc2685ffe
unknown <gunaorusty@gmail.com>
2023-09-13 13:58:54 +0800

    add sidebar content

--- dist/main.js
+++ dist/main.js
@@ -15,7 +15,7 @@
   \**********************/
 /***/ (() => {
 
-eval("\n\n//# sourceURL=webpack://todo_list/./src/index.js?");
+eval("// * Lists of nav links : Home, Today, This week and This month\r\n// * view all projects in home page\r\n// * View all todos in project when clicked\r\n// * Display title, due date and color priority type in each todo\r\n// * Add edit todo functionality\r\n// * Add delete todo functionality\r\n// * Filter todos to display in today, this week and this month\r\n// * Add sorting by date and priority in home, today, this month and this month page\n\n//# sourceURL=webpack://todo_list/./src/index.js?");
 
 /***/ })
 
---
+++ src/index.js
@@ -0,0 +1,8 @@
+// * Lists of nav links : Home, Today, This week and This month
+// * view all projects in home page
+// * View all todos in project when clicked
+// * Display title, due date and color priority type in each todo
+// * Add edit todo functionality
+// * Add delete todo functionality
+// * Filter todos to display in today, this week and this month
+// * Add sorting by date and priority in home, today, this month and this month page
